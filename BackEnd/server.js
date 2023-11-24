009d2781b63514cf9132b5cb1d49be00c7119661
Cormac Geraghty <G00409920@atu.ie>
2023-11-24 10:29:00 +0000

    lab8 complete

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -5,12 +5,12 @@ const cors = require('cors');
 
 
 app.use(cors());
-app.use(function(req, res, next) {
-res.header("Access-Control-Allow-Origin", "*");
-res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-res.header("Access-Control-Allow-Headers",
-"Origin, X-Requested-With, Content-Type, Accept");
-next();
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 const bodyParser = require("body-parser");
@@ -19,56 +19,69 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
 // getting-started.js
 const mongoose = require('mongoose');
 
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb+srv://admin:admin@martinscluster.w5rtkz0.mongodb.net/DB14?retryWrites=true&w=majority');
+  await mongoose.connect('mongodb+srv://admin:admin@cluster0.gg1uhaj.mongodb.net/?retryWrites=true&w=majority');
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
+// Defined a Schema
 const bookSchema = new mongoose.Schema({
-  title:String,
-  cover:String,
-  author:String
+  title: String,
+  cover: String,
+  author: String
 })
 
-const bookModel = mongoose.model('my_books', bookSchema);
+// Mongoose Model
+const bookModel = mongoose.model('books', bookSchema);
 
-app.post('/api/book', (req,res)=>{
-    console.log(req.body);
-
-    bookModel.create({
-      title:req.body.title,
-      cover:req.body.cover,
-      author:req.body.author
-    })
-    .then(()=>{ res.send("Book Created")})
-    .catch(()=>{ res.send("Book NOT Created")});
-
+app.put('/api/book/:id', async(req, res) =>{
+  console.log("Update: " +req.params.id)
+  let book = await bookModel.findByIdAndUpdate(req.params.id, req.body, {new:true});
+  res.send(book);
 })
 
+// Express Post Route for Creating a Book
+app.post('/api/book', (req, res) => {
+  console.log(req.body);
+  bookModel.create({
+    title: req.body.title,
+    cover: req.body.cover,
+    author: req.body.author
+  })
+    .then(() => { res.send("Book Created") })
+    .catch(() => { res.send("Book Not Created") })
+})
+
+// Default Route for HTTP GET Requests at Root Path ('/')
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/api/books', async(req, res)=>{
-    
+// Express Get Route for Fetching All Books
+app.get('/api/books', async (req, res) => {
+
   let books = await bookModel.find({});
   res.json(books);
+
 })
 
-app.get('/api/book/:identifier',async (req,res)=>{
-  console.log(req.params.identifier);
+// Express Get Route for Fetching a Specific Book by ID
+app.get('/api/book/:id', async (req, res) => {
 
-  let book = await bookModel.findById(req.params.identifier);
+  console.log(req.params.id);
+
+  let book = await bookModel.findById({ _id: req.params.id })
   res.send(book);
+
 })
 
+// Express Server Listening on a Port
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
